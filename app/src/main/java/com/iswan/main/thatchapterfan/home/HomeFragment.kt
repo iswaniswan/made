89bc0fe75f9b1ea3dd7542e9f91a772a3509827d
iswaniswan <iswan1s71@gmail.com>
2021-06-24 09:27:51 +0700

    ready to send submission 3

--- app/src/main/java/com/iswan/main/thatchapterfan/home/HomeFragment.kt
+++ app/src/main/java/com/iswan/main/thatchapterfan/home/HomeFragment.kt
@@ -2,14 +2,13 @@ package com.iswan.main.thatchapterfan.home
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.*
+import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
 import com.iswan.main.core.domain.adapters.GeneralLoadStateAdapter
 import com.iswan.main.core.domain.adapters.VideosPagingDataAdapter
@@ -19,8 +18,6 @@ import com.iswan.main.thatchapterfan.detail.DetailActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import kotlin.properties.ReadWriteProperty
-import kotlin.reflect.KProperty
 
 
 @AndroidEntryPoint
